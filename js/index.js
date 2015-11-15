dbf8fc22acbc0d23d7829fac8cc3ab1f6187d141
JJ Song <ccs401@nyu.edu>
2015-11-15 03:33:10 -0500

    Log In
    Use email abc@gmail.com, password is password.
    Does not go to about.html

--- js/index.js
+++ js/index.js
@@ -3,3 +3,9 @@ function register() {
     var password = document.getElementById("password").value;
     var age = document.getElementById("age").value;
 }
+
+function logIn() {
+    if (document.getElementById("usermail2").value == "abc@gmail.com" && document.getElementById("password2").value == "password") {
+        window.location = "about.html";
+    }
+}
