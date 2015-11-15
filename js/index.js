4fdfc2ef0438b7aace000d7b7abaab043f8ee960
JJ Song <ccs401@nyu.edu>
2015-11-15 01:53:46 -0500

    Added JS for Register
    Does not work

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function register() {
+    var email = document.getElementByName("usermail").value();
+    var password = document.getElementByName("password").value();
+    var age = document.getElementByName("age").value();
+}
